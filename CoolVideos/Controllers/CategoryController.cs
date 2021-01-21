f22c3cdcb8962f7f016e1b5c96e4e55004201d3c
Goleniewski Damian <goleniewski.damian1@gmail.com>
2021-01-21 17:15:39 +0100

    Create CategoryController.cs

---
+++ CoolVideos/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoolVideos.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolVideos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CoolVideosContext _context;
+
+        public CategoryController(CoolVideosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
