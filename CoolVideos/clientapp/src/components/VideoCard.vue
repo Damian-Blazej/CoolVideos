d2047eaa89f75b5137a2fb18e153267df7eac3f6
Błażej Tempski <blazej33.9@gmail.com>
2021-01-21 20:16:17 +0100

    Video partial components

---
+++ CoolVideos/clientapp/src/components/VideoCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <b-card class="video-card mx-2 my-2" @click="openVideo(videoId)"
+            :title="title | crop"
+            title-tag="h5"
+            :img-src="imgSrc"
+            :img-alt="imgAlt"
+            img-top
+            bg-variant="warning"
+            text-variant="black">
+        <b-card-text>
+            <div class="d-flex justify-content-between">
+                <div class="video-card-text text-black text-sm-center">
+                    {{ author }}
+                </div>
+            </div>
+        </b-card-text>
+    </b-card>
+</template>
+
+<script>
+    export default {
+        name: "VideoCard",
+        props: {
+            title: String,
+            imgSrc: String,
+            imgAlt: String,
+            author: String,
+            videoId: Number,
+        },
+        methods: {
+            openVideo: function(id) {
+                this.$router.push({ path: `/video/${id}`});
+            },
+        },
+        filters: {
+            crop: function (value) {
+                if (!value)
+                    return '';
+                value = value.toString();
+                if (value.length > 42)
+                    return value.slice(0, 40) + "...";
+                else
+                    return value;
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
