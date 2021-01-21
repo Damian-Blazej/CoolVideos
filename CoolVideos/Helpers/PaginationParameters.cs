10c871cf5d45ad1ee13a50d528f1331197e876fd
Goleniewski Damian <goleniewski.damian1@gmail.com>
2021-01-21 19:03:46 +0100

    Paginacja

---
+++ CoolVideos/Helpers/PaginationParameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolVideos.Helpers
+{
+    public class PaginationParameters
+    {
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = value > maxPageSize ? maxPageSize : value;
+            }
+        }
+    }
+}
---
+++ CoolVideos/Helpers/UniqueRandomNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolVideos.Helpers
+{
+    public class UniqueRandomNumbers
+    {
+        public static HashSet<int> Generate(int maxListLength, int minRandomValue, int maxRandomValue)
+        {
+            Random random = new Random();
+            HashSet<int> outputSet = new HashSet<int>();
+
+            for (int i = 0; i < maxListLength; i++)
+            {
+                int number = random.Next(minRandomValue, maxRandomValue + 1);
+
+                if (!outputSet.Contains(number))
+                {
+                    outputSet.Add(number);
+                }
+            }
+
+            return outputSet;
+        }
+    }
+}
