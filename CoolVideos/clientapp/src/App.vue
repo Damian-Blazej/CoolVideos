0dcdb401f6508007922af6c692c041a4e3f960bf
Błażej Tempski <blazej33.9@gmail.com>
2021-01-21 20:13:56 +0100

    Add navbar and footer

--- CoolVideos/clientapp/src/App.vue
+++ CoolVideos/clientapp/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -30,3 +30,26 @@
   }
 }
 </style>
+-->
+<template>
+    <div id="app">
+        <Navbar />                
+        <!--<button v-on:click="logout" class="btn btn-outline-success my-2 my-sm-0">Wyloguj</button>-->
+        <router-view class="router-view mb-5"/>
+        <Footer />
+    </div>
+</template>
+
+<script>
+    import Navbar from './components/Navbar.vue'
+    import Footer from './components/Footer.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            Navbar,
+            Footer
+        },
+    }
+</script>
+
